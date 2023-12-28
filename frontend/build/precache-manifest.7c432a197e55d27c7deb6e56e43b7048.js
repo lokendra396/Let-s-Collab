a18aceb0cdae4483eb43858c567809f7a8c779e0
Lokendra Singh <singhlok396@gmail.com>
2023-12-28 17:06:18 +0530

    first commit

---
+++ frontend/build/precache-manifest.7c432a197e55d27c7deb6e56e43b7048.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff14fa6d612b68c7525d",
+    "url": "/static/css/main.00c30e45.chunk.css"
+  },
+  {
+    "revision": "ff14fa6d612b68c7525d",
+    "url": "/static/js/main.ff14fa6d.chunk.js"
+  },
+  {
+    "revision": "b13758a20033f8f97718",
+    "url": "/static/js/1.b13758a2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "46c0403b59b6620793e9ff9b2f49986b",
+    "url": "/index.html"
+  }
+];
